8e89c8aebcb50e466ae8ce01648f039bf50e36b9
Alan Gutierrez <alan@prettyrobots.com>
2013-07-12 14:21:54 -0400

    Implement rudimentary put without atomicity.
    A not at all complete sketch of a batch and iterator implementation.
    
    See #13.

--- index.js
+++ index.js
@@ -1,14 +1,124 @@
 module.exports = Locket
 
-var Strata = require('b-tree')
+var Strata            = require('b-tree')
 var AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN
+var AbstractIterator  = require('abstract-leveldown').AbstractIterator
 
 var util = require('util')
-var fs = require('fs')
+var fs   = require('fs')
 var path = require('path')
 
 var cadence = require('cadence')
-var mkdirp = require('mkdirp')
+var mkdirp  = require('mkdirp')
+
+function Iterator (db, options) {
+    this._db = db
+    this._start = options.start
+    this._limit = options.limit
+}
+util.inherits(Iterator, AbstractIterator)
+
+Iterator.prototype._getLatestGoingForward = cadence(function (step, collection) {
+    var cursor = this._cursors[collection].cursor
+    var index = this._cursors[collection].index
+    step(function () {
+        if (index < cursor.length) return true
+        else step(function () {
+            cursor.next(step())
+        }, function (more) {
+            index = cursor.index
+            return more
+        })
+    }, function (more) {
+        if (more) {step(function () {
+            cursor.get(index, step())
+        }, function (record) {
+            step(function () {
+                if (index < cursor.length) return true
+                else step(function () {
+                    cursor.next(step())
+                }, function (more) {
+                    index = cursor.index
+                    return more
+                })
+            }, function (more) {
+                if (more) cursor.get(index, step())
+                else step(null, record)
+            }, function (next) {
+                index++
+                if (next.key == record.key) record = next
+                else step(null, record)
+            })()
+        })} else {
+            return null
+        }
+    }, function (record) {
+        this._cursors[collection].index = index
+        if (record) {
+            record.transactionId = record.transactionId || 0
+            this._cursors[collection].record = record
+        } else {
+            delete this._cursors[collection].record
+        }
+        return record
+    })
+})
+
+Iterator.prototype._next = cadence(function (step) {
+    var db = this._db
+    step(function () {
+        if (!this._cursors) step(function () {
+            this._cursors = {}
+            step(function (collection) {
+                var tree = db['_' + collection]
+                step(function () {
+                    tree.iterator({ key: this._start, transactionId: 0 }, step())
+                }, function (cursor) {
+                    var index = cursor.index < 0 ? ~ cursor.index : cursor.index
+                    this._cursors[collection] = {
+                        name: collection,
+                        cursor: cursor,
+                        index: index
+                    }
+                })
+            })([ 'primary', 'secondary', 'tertiary' ])
+        }, function () {
+            ; [ 'primary', 'secondary', 'tertiary' ].forEach(step([], function (collection) {
+                this._getLatestGoingForward(collection, step())
+            }))
+        })
+    }, function () {
+        var active = [ 'primary', 'secondary', 'tertiary' ].filter(function (collection) {
+            return ('record' in this._cursors[collection])
+        }.bind(this))
+
+        var candidates = active.map(function (collection) {
+            return this._cursors[collection]
+        }.bind(this))
+
+        var key = candidates.reduce(function (previous, current) {
+            return previous.record.key < current.record.key ? previous : current
+        }).record.key
+
+        candidates = candidates.filter(function (candidate) {
+            return key == candidate.record.key
+        })
+
+        var winner = candidates.reduce(function (previous, current) {
+            return previous.record.transactionId > current.record.transactionId ? previous : current
+        })
+
+        if (winner.record.type == 'del') this._next(step())
+        else step(function () {
+            candidates.forEach(step([], function (candidate) {
+                this._getLatestGoingForward(candidate.name, step())
+            }));
+        }, function () {
+            return winner.record.value
+        })
+    })
+})
+
 
 function Locket (location) {
     if (!(this instanceof Locket)) return new Locket(location)
@@ -17,19 +127,39 @@ function Locket (location) {
 
 util.inherits(Locket, AbstractLevelDOWN)
 
+function extractKey (record) {
+    return { key: record.key }
+}
+
+function compareKey (leaf, right) {
+    if (left.key < right.key) return -1
+    else if (left.key > right.key) return 1
+    return 0
+}
+
+function extractKeyAndTransaction (record) {
+    return { key: record.key, transactionId: record.transactionId }
+}
+
+function compareKeyAndTransaction (left, right) {
+    if (left.key < right.key) return -1
+    else if (left.key > right.key) return 1
+    return left.transactionId - right.transactionId
+}
+
 Locket.prototype._open = cadence(function (step, options) {
     var exists = true
     step(function () {
-        step([function () {
+        var readdir = step([function () {
             fs.readdir(this.location, step())
         }, 'ENOENT', function (_, error) {
             if (options.createIfMissing) {
                 exists = false
-                mkdirp(this.location, step())
+                mkdirp(this.location, step(readdir, 0))
             } else {
                 throw new Error('does not exist')
             }
-        }])(2)
+        }])(1)
     }, function (listing) {
         var subdirs = [ 'primary', 'secondary', 'tertiary', 'transactions' ]
         if (exists) {
@@ -43,14 +173,152 @@ Locket.prototype._open = cadence(function (step, options) {
           }))
         }
     }, function () {
-        this._primary = new Strata(path.join(this.location, 'primary'), {
+        this._primary = new Strata({
+            directory: path.join(this.location, 'primary'),
+            extractor: extractKey,
+            comparator: compareKey,
             leafSize: 1024,
             branchSize: 1024
         })
         if (!exists) this._primary.create(step())
+        this._secondary = new Strata({
+            directory: path.join(this.location, 'secondary'),
+            extractor: extractKeyAndTransaction,
+            comparator: compareKeyAndTransaction,
+            leafSize: 1024,
+            branchSize: 1024
+        })
+        if (!exists) this._secondary.create(step())
+        this._tertiary = new Strata({
+            directory: path.join(this.location, 'tertiary'),
+            extractor: extractKeyAndTransaction,
+            comparator: compareKeyAndTransaction,
+            leafSize: 1024,
+            branchSize: 1024
+        })
+        if (!exists) this._tertiary.create(step())
+        this._transactions = new Strata({
+            directory: path.join(this.location, 'transactions'),
+            extractor: extractKey,
+            leafSize: 1024,
+            branchSize: 1024
+        })
+        if (!exists) this._transactions.create(step())
     }, function () {
         this._primary.open(step())
+        this._secondary.open(step())
+        this._tertiary.open(step())
+        this._transactions.open(step())
+    }, function () {
         this._isOpened = true
+        this._operations = 0
+        this._successfulTransactions = {}
+        this._nextTransactionId = 0
+        this._staging = this._secondary
+        this._transactions.iterator(step())
+    }, function (transactions) {
+        step(function (more) {
+            if (!more) {
+                transactions.unlock()
+                step(null)
+            }
+        }, function () {
+            var offset = transactions.offset
+            var length = transactions.length
+            step(function (i) {
+                cursor.get(i + offset, step())
+            }, function (transactionId) {
+                this._successfulTransactions[transactionId] = true
+                this._nextTransactionId = Math.max(this._nextTransactionId, transactionId + 1)
+            })(length - offset)
+        }, function () {
+            transactions.next(step())
+        })(null, true)
+    })
+})
+
+var operations = {
+    put: cadence(function (step, transaction, options, operation) {
+        var staging = this._staging
+        var record = {
+            transactionId: transaction.id,
+            key: operation.key,
+            value: operation.value
+        }
+        step(function () {
+            if (transaction.cursor) {
+                return transaction.cursor
+            } else {
+                staging.mutator(record, step())
+            }
+        }, function (cursor) {
+            step(function () {
+                if (transaction.cursor) {
+                    cursor.indexOf(record, step())
+                } else {
+                    transaction.cursor = cursor
+                    return cursor.index
+                }
+            }, function (index) {
+                if (index < 0) step(function () {
+                    cursor.insert(record, record, ~ index, step())
+                }, function (insert) {
+                    if (insert != 0) {
+                        delete transaction.cursor
+                        cursor.unlock()
+                        operations.put(call, this, transaction, options, operation)
+                    } else {
+                        this._operations++
+                    }
+                }) // otherwise it's a duplicate 'put', return early
+            })
+        })
+    })
+}
+
+Locket.prototype._get = cadence(function (step, key, options) {
+    var iterator = new Iterator(this, { start: key, limit: 1 })
+    step(function () {
+        iterator.next(step())
+    }, function (value) {
+        console.log(key)
+        step(function () { iterator.end(step()) }, function () { return value })
+    })
+})
+
+Locket.prototype._put = function (key, value, options, callback) {
+    if (!callback) {
+        callback = options
+        options = {}
+    }
+    this._batch([{ type: 'put', key: key, value: value }], options, callback)
+}
+
+Locket.prototype._del = function (step, key, options, callback) {
+    if (!callback) {
+        callback = options
+        options = {}
+    }
+    this._batch([{ type: 'del', key: key }], options, callback)
+}
+
+Locket.prototype._batch = cadence(function (step, array, options) {
+    var transaction = { id: this._nextTransactionId++ }
+    step(function () {
+        step(function (operation) {
+            operations[operation.type].call(this, transaction, options, operation, step())
+        })(array)
+    }, function () {
+        if (transaction.cursor) transaction.cursor.unlock()
+        step(function () {
+            this._transactions.mutator(transaction.id, step())
+        }, function (cursor) {
+            step(function () {
+                cursor.insert(transaction.id, transaction.id, ~ cursor.index, step())
+            }, function () {
+                cursor.unlock()
+            })
+        })
     })
 })
 
@@ -61,3 +329,5 @@ Locket.prototype._close = cadence(function (step, operations) {
         this._isOpened = false
     })
 })
+
+// vim: sw=4 ts=4:
---
+++ t/locket/put.t.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+require('proof')(1, function (step, equal, deepEqual) {
+    var path = require('path')
+    var fs = require('fs')
+
+    var rimraf = require('rimraf')
+    var cadence = require('cadence')
+
+    var Locket = require('../..')
+
+    var tmp = path.join(__dirname, '../tmp')
+
+    step(function () {
+        var location = path.join(tmp, 'put')
+        var locket
+        step(function () {
+            rimraf(location, step())
+        }, function () {
+            locket = new Locket(location)
+            locket.open({ createIfMissing: true }, step())
+        }, function () {
+            locket.put('a', JSON.stringify({ value: 1 }), step())
+        }, function () {
+            locket.get('a', step())
+        }, function (got) {
+            deepEqual(JSON.parse(got), { value: 1 }, 'put')
+        })
+    })
+})
