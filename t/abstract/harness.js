4eab9b3f18f1cbd24eaeda8de521aca5bc49909b
Alan Gutierrez <alan@prettyrobots.com>
2013-08-01 02:08:58 -0400

    Invoke AbstractLevelDOWN tests from Proof.
    Closes #38.

---
+++ t/abstract/harness.js
@@ -0,0 +1,11 @@
+module.exports = function (suite, tests) {
+    var test        = require('tap').test
+    var testCommon  = require('abstract-leveldown/testCommon')
+    var Locket      = require('../..')
+
+    function factory (location) { return new Locket(location) }
+
+    var path = 'abstract-leveldown/abstract/' + suite + '-test'
+
+    require(path)[tests](factory, test, testCommon)
+}
