49d6b9378492d64ccccb5eb0b3061d3b60e67e31
Alan Gutierrez <alan@prettyrobots.com>
2015-02-05 23:37:19 -0600

    More specific raw benchmarks.
    Add specific benchmarks for write and read, instead of using the UNIX
    `time` command to time the benchmark.
    
    See #138.

--- benchmark/load.js
+++ benchmark/load.js
@@ -33,41 +33,50 @@ var random = (function () {
 })()
 
 var runner = cadence(function (async, options) {
+    var start, insert, gather
     var file = path.join(__dirname, 'tmp', 'put'), db, count = 0
     var o = { createIfMissing: true }
     if (!options.params.leveldown) {
         o.db = require('..')
+    }
+    var batches = []
+    while (batches.length != 7) {
+        var entries = []
+        var type, sha, buffer, value
+        for (var i = 0; i < 1024; i++) {
+            var value = random(10000)
+            sha = crypto.createHash('sha1')
+            buffer = new Buffer(4)
+            buffer.writeUInt32BE(value, 0)
+            sha.update(buffer)
+            entries.push({
+                key: sha.digest(),
+                value: buffer,
+                type: !! random(2) ? 'put' : 'del'
+            })
+        }
+        batches.push(entries)
     }
     async(function () {
         rimraf(file, async())
     }, function () {
         //mkdirp(file, async())
     }, function () {
+        start = Date.now()
         levelup(file, o, async())
     }, function (db) {
         async(function () {
             var batch = 0, loop = async(function () {
-                if (batch++ == 7) return [ loop ]
-                var entries = []
-                var type, sha, buffer, value
-                for (var i = 0; i < 1024; i++) {
-                    var value = random(10000)
-                    sha = crypto.createHash('sha1')
-                    buffer = new Buffer(4)
-                    buffer.writeUInt32BE(value, 0)
-                    sha.update(buffer)
-                    entries.push({
-                        key: sha.digest(),
-                        value: buffer,
-                        type: !! random(2) ? 'put' : 'del'
-                    })
-                }
-                db.batch(entries, async())
+                if (batch == 7) return [ loop ]
+                db.batch(batches[batch], async())
+                batch++
             })()
         }, function () {
             db.close(async())
         })
     }, function () {
+        insert = Date.now() - start
+        start = Date.now()
         levelup(file, o, async())
     }, function (db) {
         async(function () {
@@ -76,8 +85,10 @@ var runner = cadence(function (async, options) {
                  .end('end')
                  .error()
         }, function () {
-            console.log('count', count)
             db.close(async())
+        }, function () {
+            gather = Date.now() - start
+            console.log('insert: ' + insert + ', gather: ' + gather)
         })
     })
 })
