389b96e6d9a6a97f6004f75c3909a7e5a548f063
Alan Gutierrez <alan@prettyrobots.com>
2015-04-13 11:31:14 -0500

    Delete unused `test.js` crufty.

--- test.js
+++
@@ -1,7 +0,0 @@
-var test = require('tap').test
-var testCommon = require('abstract-leveldown/testCommon')
-var Locket = require('./index')
-var factory = function (location) { return new Locket(location) }
-
-require('abstract-leveldown/abstract/leveldown-test').args(factory, test, testCommon)
-require('abstract-leveldown/abstract/open-test').all(factory, test, testCommon)
