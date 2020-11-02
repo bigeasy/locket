b2d82729fd192f1dd3759ff6660e4b37b9f58433
Alan Gutierrez <alan@prettyrobots.com>
2020-11-02 12:56:21 -0600

    Convert to Cadence.
    Since we're implementing an error-first callback interface, let's do so.
    I've added promise support to Cadence, and error-first callback support
    to Reciprocate. Now there ought to be no unnecessary escapes to the
    Node.js event loop or the promise event loop. Anything that can be run
    synchrnously will be run synchronously using the trampoline in Cadence
    or the trampoline in Reciprocate.
    
    Closes #249.

--- test/locket.t.js
+++ test/locket.t.js
@@ -1,4 +1,4 @@
-require('proof')(6, async okay =>  {
+require('proof')(6, require('cadence')(function (step, okay) {
     const path = require('path')
     const fs = require('fs')
 
@@ -12,100 +12,115 @@ require('proof')(6, async okay =>  {
 
     const location = path.join(__dirname, 'tmp', 'locket')
 
-    await callback(callback => rimraf(location, callback))
+    step(function () {
+        fs.rmdir(location, { recursive: true }, step())
+    }, function () {
+        new Locket(destructible, location)
 
-    new Locket(destructible, location)
+        const locket = Locket(destructible, location, {
+            primary: {
+                leaf: { split: 64, merge: 32 },
+                branch: { split: 64, merge: 32 },
+            },
+            stage: {
+                max: 128,
+                leaf: { split: 64, merge: 32 },
+                branch: { split: 64, merge: 32 },
+            }
+        })
 
-    const locket = Locket(destructible, location, {
-        primary: {
-            leaf: { split: 64, merge: 32 },
-            branch: { split: 64, merge: 32 },
-        },
-        stage: {
-            max: 128,
-            leaf: { split: 64, merge: 32 },
-            branch: { split: 64, merge: 32 },
-        }
-    })
-    await callback(callback => locket.open({ createIfMissing: true }, callback))
-
-    await callback(callback => locket.put('a', 'z', callback))
-
-    {
-        const [ value ] = await callback(callback => locket.get('a', callback))
-        okay({
-            isBuffer: Buffer.isBuffer(value),
-            value: value.toString()
-        }, {
-            isBuffer: true,
-            value: 'z'
-        }, 'put')
-    }
-
-    {
-        const [ value ] = await callback(callback => locket.get('a', { asBuffer: false }, callback))
-        okay({
-            type: typeof value,
-            value: value
-        }, {
-            type: 'string',
-            value: 'z'
-        }, 'get')
-    }
-
-    await callback(callback => locket.del('a', callback))
-
-    {
-        const test = []
-        try {
-            await callback(callback => locket.get('a', callback))
-        } catch (error) {
-            test.push(error.message)
-        }
-        okay(test, [ 'NotFoundError: not found' ], 'get not found')
-    }
-
-    await callback(callback => locket.put(Buffer.from('a'), Buffer.from('z'), callback))
-
-    await callback(callback => locket.close(callback))
-
-    await Destructible.rescue(async function () {
-        await callback(callback => locket.open({ createIfMissing: true }, callback))
-
-        {
-            const [ value ] = await callback(callback => locket.get('a', callback))
-            okay({
-                isBuffer: Buffer.isBuffer(value),
-                value: value.toString()
-            }, {
-                isBuffer: true,
-                value: 'z'
-            }, 'reopen')
-        }
-
-        {
+        step(function () {
+            locket.open({ createIfMissing: true }, step())
+        }, function () {
+            locket.put('a', 'z', step())
+        }, function () {
+            step(function () {
+                locket.get('a', step())
+            }, function (value) {
+                okay({
+                    isBuffer: Buffer.isBuffer(value),
+                    value: value.toString()
+                }, {
+                    isBuffer: true,
+                    value: 'z'
+                }, 'put')
+            })
+        }, function () {
+            step(function () {
+                locket.get('a', { asBuffer: false }, step())
+            }, function (value) {
+                okay({
+                    type: typeof value,
+                    value: value
+                }, {
+                    type: 'string',
+                    value: 'z'
+                }, 'get')
+            })
+        }, function () {
+            locket.del('a', step())
+        }, function () {
+            const test = []
+            step([function () {
+                locket.get('a', step())
+            }, function (error) {
+                test.push(error.message)
+            }], function () {
+                okay(test, [ 'NotFoundError: not found' ], 'get not found')
+            })
+        }, function () {
+            locket.put(Buffer.from('a'), Buffer.from('z'), step())
+        }, function () {
+            locket.close(step())
+        }, function () {
+            locket.open({ createIfMissing: true }, step())
+        }, function () {
+            step(function () {
+                locket.get('a', step())
+            }, function (value) {
+                okay({
+                    isBuffer: Buffer.isBuffer(value),
+                    value: value.toString()
+                }, {
+                    isBuffer: true,
+                    value: 'z'
+                }, 'reopen')
+            })
+        }, function () {
             const iterator = locket._iterator({
                 keys: true, values: true, keyAsBuffer: false, valueAsBuffer: false
             })
-            const [ key, value ] = await callback(callback => iterator.next(callback))
-            okay({ key, value }, { key: 'a', value: 'z' }, 'next')
-            const ended = await callback(callback => iterator.next(callback))
-            okay(ended, [], 'ended')
-            await callback(callback => iterator.end(callback))
-        }
+            step(function () {
+                iterator.next(step())
+            }, function (key, value) {
+                okay({ key, value }, { key: 'a', value: 'z' }, 'next')
+                iterator.next(step())
+            }, [], function (ended) {
+                okay(ended, [], 'ended')
+                iterator.end(step())
+            })
+        }, function () {
+            const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
 
-        const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+            const put = alphabet.map(letter => { return { type: 'put', key: letter, value: letter } })
+            const del = alphabet.map(letter => { return { type: 'del', key: letter } })
 
-        const put = alphabet.map(letter => { return { type: 'put', key: letter, value: letter } })
-        const del = alphabet.map(letter => { return { type: 'del', key: letter } })
-
-        for (let i = 0; i < 128; i++) {
-            await callback(callback => locket.batch(put.concat(del), callback))
-        }
+            step.loop([ 0 ], function (i) {
+                if (i == 128) {
+                    return [ step.break ]
+                }
+                step(function () {
+                    locket.batch(put.concat(del), step())
+                }, function () {
+                    return i + 1
+                })
+            })
+        }, function () {
+            locket.close(step())
+        }, function () {
+            locket.open({ createIfMissing: true }, step())
+        }, function () {
+            locket.close(step())
+        })
     })
-
-    await callback(callback => locket.close(callback))
-
-    await callback(callback => locket.open({ createIfMissing: true }, callback))
-    await callback(callback => locket.close(callback))
-})
+}))
